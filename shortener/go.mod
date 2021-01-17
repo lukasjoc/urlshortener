fc2f19bcdea5ac437c0a6bbf821d22909e0c4ad3
lukasjoc <jochamlu@gmail.com>
2021-01-17 14:33:14 +0100

    add utility script

--- shortener/go.mod
+++ shortener/go.mod
@@ -3,12 +3,21 @@ module gitub.com/urlshortener
 go 1.15
 
 require (
-	github.com/facebook/ent v0.5.4
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-sql-driver/mysql v1.5.1-0.20200311113236-681ffa848bae // indirect
 	github.com/golang/protobuf v1.4.1
 	github.com/jinzhu/gorm v1.9.16
-	github.com/json-iterator/go v1.1.10
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lib/pq v1.9.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.6 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/sirupsen/logrus v1.7.0
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.25.0
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- shortener/server.go
+++ shortener/server.go
@@ -7,18 +7,31 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/sirupsen/logrus"
 	"gitub.com/urlshortener/proto"
 )
 
-// URLModel
+// URLModel ...
 type URLModel struct {
 	gorm.Model
-	proto.Url
+	// proto.Url
 }
 
 // test http://localhost:5050/Tr-8mZNJ
 func main() {
+	const addrs = "postgres://root:admin@127.0.0.1:53232/urls?sslmode=require"
+	db, err := gorm.Open("postgres", addrs)
+	if err != nil {
+		logrus.Fatal(err)
+	} else {
+		logrus.Info("Connected to cockroach db")
+	}
+
+	defer db.Close()
+	db.LogMode(false)
+	db.AutoMigrate(&URLModel{})
+
 	router := gin.Default()
 	router.GET("/:url", func(ctx *gin.Context) {
 		// TODO:  find url
@@ -26,17 +39,18 @@ func main() {
 			"message": "Not Implemented",
 		})
 	})
+
 	router.PUT("/save", func(ctx *gin.Context) {
-		// TODO: save to db
 		url := &proto.Url{}
 		ctx.BindJSON(url)
+		db.Create(url)
 		ctx.JSON(http.StatusOK, gin.H{
 			"generated": url.GetGenerated(),
 			"ip_v4":     url.GetIpV4(),
 			"real":      url.GetReal(),
 		})
-
 	})
+
 	if os.Getenv("GIN_MODE") == "release" {
 		gin.SetMode(gin.ReleaseMode)
 	}
